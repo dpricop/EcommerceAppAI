74f96686b5f15a24a73f261b2d10987ff7a9a319
dpricop <pricopdumitru94@gmail.com>
2025-06-26 16:15:58 +0300

    .net8 MVC template app

---
+++ EcommerceAppAI/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace EcommerceAppAI.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
