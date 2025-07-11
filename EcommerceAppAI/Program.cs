9037985bfb8e4866d2a420da7ad7ae70d0dac552
dpricop <pricopdumitru94@gmail.com>
2025-07-11 12:48:05 +0300

    N/I

--- EcommerceAppAI/Program.cs
+++ EcommerceAppAI/Program.cs
@@ -41,9 +41,4 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
-app.MapControllerRoute(
-    name: "qdrant-test",
-    pattern: "qdrant-test/{action=Index}",
-    defaults: new { controller = "QdrantTest" });
-
 app.Run();
