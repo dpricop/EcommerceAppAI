7e27d1022d4572e3a8d14c34ad69693e5639a2e9
dpricop <pricopdumitru94@gmail.com>
2025-07-22 20:19:23 +0300

    LM Studio has been replaced with Ollama service.

--- EcommerceAppAI/Models/LlmSettings.cs
+++ EcommerceAppAI/Models/LlmSettings.cs
@@ -2,8 +2,9 @@ namespace EcommerceAppAI.Models;
 
 public class LlmSettings
 {
-    public string BaseUrl { get; set; } = "http://127.0.0.1:1234";
-    public string ModelName { get; set; } = "llama-3.2-3b-instruct";
+    public string BaseUrl { get; set; } = "http://localhost:11434";
+    public string ModelName { get; set; } = "llama3.2:latest";
+    public string EmbeddingModel { get; set; } = "nomic-embed-text";
     public double Temperature { get; set; } = 0.7;
     public int MaxTokens { get; set; } = 1000;
     public int Timeout { get; set; } = 30;
